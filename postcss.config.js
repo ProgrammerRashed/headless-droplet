d6370a1b2111ce7b3eefd1d8f854680c0cb34740
Abdul Muhaimin Toha <toha@notionhive.com>
2024-12-09 04:49:09 +0600

    next config changed

--- postcss.config.js
+++ postcss.config.js
@@ -3,26 +3,4 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
-  async headers() {
-    return [
-      {
-        // Match media files (adjust the path or extensions as needed)
-        source: "/media/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
-          },
-          {
-            key: "Pragma",
-            value: "no-cache",
-          },
-          {
-            key: "Expires",
-            value: "0",
-          },
-        ],
-      },
-    ];
-  },
 };

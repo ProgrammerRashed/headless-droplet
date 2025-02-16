e9d3f6a87210c54c666efea16948eb84694aa889
Rashed <rashed@notionhive.com>
2025-02-16 12:32:38 +0600

    Updated contact from

--- components/contactUsPage/contactFromSection/ContactFromSection.jsx
+++ components/contactUsPage/contactFromSection/ContactFromSection.jsx
@@ -1,3 +1,4 @@
+"use client"
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -9,38 +10,89 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import useSendMail from "@/utils/useSendEmail";
+import { useState } from "react";
 
 function ContactFromSection() {
+  const { sendMail, loading, message, error, setMessage, setError } = useSendMail();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    phone: "",
+    email: "",
+    country: "",
+    inquiryType: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  const handleSelectChange = (id, value) => {
+    setFormData((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+       await sendMail(formData)
+    console.log("Form Data Submitted:", formData);
+  };
+
+
+  const inquiryTypes = [
+    { value: "General Inquiry", label: "General Inquiry" },
+    { value: "Partnership & Collaboration", label: "Partnership & Collaboration" },
+    { value: "Funding & Grants", label: "Funding & Grants" },
+    { value: "Volunteering & Internships", label: "Volunteering & Internships" },
+    { value: "Educational Programs & Workshops", label: "Educational Programs & Workshops" },
+    { value: "Media & Press", label: "Media & Press" },
+    { value: "Research & Publications", label: "Research & Publications" },
+    { value: "Donations & Sponsorships", label: "Donations & Sponsorships" },
+    { value: "Technical Assistance & Support", label: "Technical Assistance & Support" },
+    { value: "Feedback & Suggestions", label: "Feedback & Suggestions" },
+  ];
   return (
     <section className="bg-white py-10 md:py-[100px]">
       <div className="container !max-w-[1168px]">
-        <form className="grid grid-cols-1 gap-5 border border-gray-50/10 px-[15px] py-[30px] shadow-md shadow-gray-200 md:grid-cols-2 md:p-[30px] lg:p-[60px]">
+        <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-5 border border-gray-50/10 px-[15px] py-[30px] shadow-md shadow-gray-200 md:grid-cols-2 md:p-[30px] lg:p-[60px]">
           <div data-aos="fade-up" className="w-full flex-col gap-1">
             <Label
-              htmlFor="first-name"
+              htmlFor="firstName"
               className="text-base font-normal capitalize leading-[26px] text-gray-800"
             >
               First name <span className="text-red-500">*</span>
             </Label>
             <Input
               type="text"
-              id="first-name"
+              id="firstName"
               placeholder="First name"
               className="m-0 h-auto w-full rounded-[40px] border border-gray-50 bg-surface px-4 py-3 text-sm leading-[22px] text-gray-700 shadow-none placeholder:text-gray-400 focus-visible:ring-green-500"
+              value={formData.firstName}
+              onChange={handleChange}
             />
           </div>
           <div data-aos="fade-up" className="w-full flex-col gap-1">
             <Label
-              htmlFor="last-name"
+              htmlFor="lastName"
               className="text-base font-normal capitalize leading-[26px] text-gray-800"
             >
               Last name <span className="text-red-500">*</span>
             </Label>
             <Input
               type="text"
-              id="last-name"
+              id="lastName"
               placeholder="Last name"
               className="m-0 h-auto w-full rounded-[40px] border border-gray-50 bg-surface px-4 py-3 text-sm leading-[22px] text-gray-700 shadow-none placeholder:text-gray-400 focus-visible:ring-green-500"
+              value={formData.lastName}
+              onChange={handleChange}
             />
           </div>
           <div data-aos="fade-up" className="w-full flex-col gap-1">
@@ -55,6 +107,8 @@ function ContactFromSection() {
               id="phone"
               placeholder="Phone number"
               className="m-0 h-auto w-full rounded-[40px] border border-gray-50 bg-surface px-4 py-3 text-sm leading-[22px] text-gray-700 shadow-none placeholder:text-gray-400 focus-visible:ring-green-500"
+              value={formData.phone}
+              onChange={handleChange}
             />
           </div>
           <div data-aos="fade-up" className="w-full flex-col gap-1">
@@ -69,6 +123,8 @@ function ContactFromSection() {
               id="email"
               placeholder="Email"
               className="m-0 h-auto w-full rounded-[40px] border border-gray-50 bg-surface px-4 py-3 text-sm leading-[22px] text-gray-700 shadow-none placeholder:text-gray-400 focus-visible:ring-green-500"
+              value={formData.email}
+              onChange={handleChange}
             />
           </div>
           <div data-aos="fade-up" className="w-full flex-col gap-1">
@@ -78,7 +134,7 @@ function ContactFromSection() {
             >
               Country <span className="text-red-500">*</span>
             </Label>
-            <Select className="w-full">
+            <Select onValueChange={(value) => handleSelectChange("country", value)} className="w-full">
               <SelectTrigger
                 id="country"
                 className="m-0 h-auto w-full rounded-[40px] border border-gray-50 bg-surface px-4 py-3 text-sm leading-[22px] text-gray-700 shadow-none placeholder:text-gray-400 focus:ring-green-500"
@@ -122,7 +178,7 @@ function ContactFromSection() {
             >
               Inquiry type <span className="text-red-500">*</span>
             </Label>
-            <Select className="w-full">
+            <Select onValueChange={(value) => handleSelectChange("inquiryType", value)} className="w-full">
               <SelectTrigger
                 id="inquiry-type"
                 className="m-0 h-auto w-full rounded-[40px] border border-gray-50 bg-surface px-4 py-3 text-sm leading-[22px] text-gray-700 shadow-none placeholder:text-gray-400 focus:ring-green-500"
@@ -131,30 +187,16 @@ function ContactFromSection() {
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
-                  <SelectItem
-                    value="type-1"
-                    className="text-gray-700 hover:bg-surface focus:bg-surface"
-                  >
-                    Type placeholder
-                  </SelectItem>
-                  <SelectItem
-                    value="type-2"
-                    className="text-gray-700 hover:bg-surface focus:bg-surface"
-                  >
-                    Type placeholder
-                  </SelectItem>
-                  <SelectItem
-                    value="type-3"
-                    className="text-gray-700 hover:bg-surface focus:bg-surface"
-                  >
-                    Type placeholder
-                  </SelectItem>
-                  <SelectItem
-                    value="type-4"
-                    className="text-gray-700 hover:bg-surface focus:bg-surface"
-                  >
-                    Type placeholder
-                  </SelectItem>
+                  {inquiryTypes.map((type) => (
+                    <SelectItem
+                      key={type.value}
+                      value={type.value}
+                      className="text-gray-700 hover:bg-surface focus:bg-surface"
+                    >
+                      {type.label}
+                    </SelectItem>
+
+                  ))}
                 </SelectGroup>
               </SelectContent>
             </Select>
@@ -173,13 +215,16 @@ function ContactFromSection() {
               placeholder="Write here..."
               id="message"
               className="scrollbar-thin m-0 h-auto w-full rounded-[10px] border border-gray-50 bg-surface px-4 py-3 text-sm leading-[22px] text-gray-700 shadow-none placeholder:text-gray-400 focus-visible:ring-green-500"
+              value={formData.message}
+              onChange={handleChange}
             />
           </div>
           <button
+            type="submit"
             data-aos="fade-up"
             className="-mt-1 flex w-fit flex-row items-center gap-2 rounded-full border border-red-600 bg-red-600 stroke-white px-6 py-[13px] text-sm font-semibold capitalize leading-[22px] text-white transition-all duration-300 hover:bg-red-900 md:col-span-2 md:px-6 md:py-[15px] md:text-base md:leading-[24px]"
           >
-            <span>Submit now</span>
+            <span>{loading ? "Submitting..." : "Send Email"}</span>
             <svg
               width={20}
               height={20}
@@ -203,6 +248,13 @@ function ContactFromSection() {
               </defs>
             </svg>
           </button>
+          {
+            message && (
+              <div className="mt-5 text-base font-normal text-gray-800">
+                {message}
+              </div>
+            )
+          }
         </form>
       </div>
     </section>

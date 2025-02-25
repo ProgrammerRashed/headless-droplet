1ddd670a07eb6e1b9eb859e90ce81f006e7c05b5
Rashed <rashed@notionhive.com>
2025-02-25 11:34:34 +0600

    Added helper console to indentify rendered blocks

--- utils/RenderBlocksHelper.js
+++ utils/RenderBlocksHelper.js
@@ -5,15 +5,20 @@ const RenderBlocksHelper = ({ blocks }) => {
     return <p>Page is empty</p>;
   }
 
-
   return (
     <div>
       {blocks.map((block, index) => {
         const BlockComponent = blockComponentsMapping[block.name];
 
         if (!BlockComponent) {
-          console.log(`Block with name "${block.name}" is not found in Block Mapping`);
+          if (process.env.NODE_ENV === 'development') {
+            console.log(`Block with name "${block.name}" is not found in Block Mapping`);
+          }
           return null;
+        }
+
+        if (process.env.NODE_ENV === 'development') {
+          console.log(`Rendering block: ${block.name}`);
         }
 
         return <BlockComponent key={index} data={JSON.parse(block.attributesJSON)} />;

d4936dd0be9c0c7802d06deb7a3c3cc0a5dada7b
Rashed <tools.rashed@gmail.com>
2025-03-20 15:38:29 +0600

    feat: added dynamic seo title

--- app/(countriesLayout)/bangladesh/contact-us/page.jsx
+++ app/(countriesLayout)/bangladesh/contact-us/page.jsx
@@ -1,5 +1,47 @@
 import { getPageData } from "@/graphql/Components";
+import { getSeoData } from "@/graphql/Components/getSeoData";
 import RenderBlocksHelper from "@/utils/RenderBlocksHelper";
+
+
+export async function generateMetadata() {
+  const data = await getSeoData("/bangladesh-contact-us-2") || {};
+  
+  const { seo = {} } = data;
+  const {
+    title = "Sustainable Agricultural Foundation",
+    metaDesc = "The Sustainable Agriculture Foundation (SAF), formerly SFSA, builds on a 40-year legacy of transforming smallholder agriculture in Asia and Africa, focusing on climate-smart innovations, market access, and sustainable practices.",
+    metaKeywords = "saf, SAF, Sustainable Agricultural, Sustainable Agricultural Foundation, saf bangladesh, saf kenya",
+    canonical = "susagfoundation.org",
+    opengraphImage = {},
+    featuredImage = {}
+  } = seo;
+
+  const defaultImageUrl = "https://dashboard.susagfoundation.org/wp-content/uploads/2025/02/image1-2.png";
+  const twitterImageUrl = "https://dashboard.susagfoundation.org/wp-content/uploads/2025/02/Blog-Thrumbnai-2.png";
+
+  // Use defaultImageUrl or fall back to featured or opengraph image
+  const ogImageUrl = opengraphImage?.mediaItemUrl || featuredImage?.node?.mediaItemUrl || defaultImageUrl;
+
+  return {
+    title,
+    description: metaDesc,
+    metaDesc,
+    metaKeywords,
+    canonical,
+    openGraph: {
+      title,
+      description: metaDesc,
+      image: ogImageUrl,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description: metaDesc,
+      image: twitterImageUrl,
+    },
+    searchImage: ogImageUrl,
+  };
+}
 
 async function BangladeshContactUspage() {
     const pageData = await getPageData("/bangladesh-contact-us-2");
